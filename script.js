8eacacfa202f0b36171a5a46828a8a7655ff7234
Yogadharshni <yogdharshni06@gmail.com>
2023-02-04 19:58:53 +0530

    first commit

---
+++ script.js
@@ -0,0 +1,33 @@
+setTimeout(()=>{
+    document.getElementById("timer").innerText=10;
+    setTimeout(()=>{
+        document.getElementById("timer").innerText=9;
+        setTimeout(()=>{
+            document.getElementById("timer").innerText=8;
+            setTimeout(()=>{
+                document.getElementById("timer").innerText=7;
+                setTimeout(()=>{
+                    document.getElementById("timer").innerText=6;
+                    setTimeout(()=>{
+                        document.getElementById("timer").innerText=5;
+                        setTimeout(()=>{
+                            document.getElementById("timer").innerText=4;
+                            setTimeout(()=>{
+                                document.getElementById("timer").innerText=3;
+                                setTimeout(()=>{
+                                    document.getElementById("timer").innerText=2;
+                                    setTimeout(()=>{
+                                        document.getElementById("timer").innerText=1;
+                                        setTimeout(()=>{
+                                            document.getElementById("timer").innerText="Happy Independence Day 🎉!!";
+                                        },1000)
+                                    },1000)
+                                },1000)
+                            },1000)
+                        },1000)
+                    },1000)
+                },1000)
+            },1000)
+        },1000)
+    },1000)
+})
